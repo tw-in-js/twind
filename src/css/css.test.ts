9bdd6b6d794735e3f69f4b1c0dd70b3b6a51c066
Sascha Tandel <s.tandel@kenoxa.de>
2021-01-17 21:15:47 +0100

    feat: support template literals for css and keyframes

--- src/css/css.test.ts
+++ src/css/css.test.ts
@@ -46,19 +46,19 @@ test('create css', ({ css, tw, sheet }) => {
   // It is lazy
   assert.equal(sheet.target, [])
 
-  assert.is(tw(style), 'tw-5fqnnd')
+  assert.is(tw(style), 'tw-af5k28')
 
   assert.equal(sheet.target, [
-    '.tw-5fqnnd:hover{color:darkgreen}',
-    '.tw-5fqnnd{background-color:hotpink}',
+    '.tw-af5k28:hover{color:darkgreen}',
+    '.tw-af5k28{background-color:hotpink}',
   ])
 
   // it is cached
-  assert.is(tw(style), 'tw-5fqnnd')
+  assert.is(tw(style), 'tw-af5k28')
 
   assert.equal(sheet.target, [
-    '.tw-5fqnnd:hover{color:darkgreen}',
-    '.tw-5fqnnd{background-color:hotpink}',
+    '.tw-af5k28:hover{color:darkgreen}',
+    '.tw-af5k28{background-color:hotpink}',
   ])
 })
 
@@ -70,22 +70,9 @@ test('create with global css within custom tw', ({ tw, sheet }) => {
   // It is lazy
   assert.equal(sheet.target, [])
 
-  assert.is(tw(style), 'tw-1yoxc1q')
+  assert.is(tw(style), 'tw-1gewvs9')
 
-  assert.equal(sheet.target, ['.tw-1yoxc1q{background-color:darkgreen}'])
-})
-
-test('create with global css and call with tw', ({ tw, sheet }) => {
-  const style = css({
-    backgroundColor: 'darkgreen',
-  })
-
-  // It is lazy
-  assert.equal(sheet.target, [])
-
-  assert.is(style({ tw }), 'tw-1yoxc1q')
-
-  assert.equal(sheet.target, ['.tw-1yoxc1q{background-color:darkgreen}'])
+  assert.equal(sheet.target, ['.tw-1gewvs9{background-color:darkgreen}'])
 })
 
 test('nested selectors', ({ tw, sheet }) => {
@@ -104,26 +91,6 @@ test('nested selectors', ({ tw, sheet }) => {
 
   assert.equal(sheet.target, ['.tw-1bxgl9r a:hover{color:#1d4ed8}', '.tw-1bxgl9r a{color:#3b82f6}'])
 })
-test('cached by its JSON representation', ({ css, sheet }) => {
-  const style = css({
-    backgroundColor: 'hotpink',
-    '&:hover': {
-      color: 'darkgreen',
-    },
-  })
-
-  assert.is(
-    css({
-      backgroundColor: 'hotpink',
-      '&:hover': {
-        color: 'darkgreen',
-      },
-    }),
-    style,
-  )
-
-  assert.equal(sheet.target, [])
-})
 
 test('can be used with variants', ({ tw, sheet }) => {
   const style = css({
@@ -133,13 +100,13 @@ test('can be used with variants', ({ tw, sheet }) => {
     },
   })
 
-  assert.is(tw`sm:${style} focus:${style}`, 'sm:tw-5fqnnd focus:tw-5fqnnd')
+  assert.is(tw`sm:${style} focus:${style}`, 'sm:tw-af5k28 focus:tw-af5k28')
 
   assert.equal(sheet.target, [
-    '.focus\\:tw-5fqnnd:focus:hover{color:darkgreen}',
-    '.focus\\:tw-5fqnnd:focus{background-color:hotpink}',
-    '@media (min-width: 640px){.sm\\:tw-5fqnnd:hover{color:darkgreen}}',
-    '@media (min-width: 640px){.sm\\:tw-5fqnnd{background-color:hotpink}}',
+    '.focus\\:tw-af5k28:focus:hover{color:darkgreen}',
+    '.focus\\:tw-af5k28:focus{background-color:hotpink}',
+    '@media (min-width: 640px){.sm\\:tw-af5k28:hover{color:darkgreen}}',
+    '@media (min-width: 640px){.sm\\:tw-af5k28{background-color:hotpink}}',
   ])
 })
 
@@ -204,11 +171,11 @@ test('keyframes', ({ keyframes, css, tw, sheet }) => {
         animation: `${bounce} 1s ease infinite`,
       }),
     ),
-    'tw-1v80189',
+    'tw-15ziufb',
   )
   assert.equal(sheet.target, [
-    '@keyframes tw-cm8eaz{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
-    '.tw-1v80189{animation:tw-cm8eaz 1s ease infinite}',
+    '@keyframes tw-w4cbcw{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
+    '.tw-15ziufb{animation:tw-w4cbcw 1s ease infinite}',
   ])
 })
 
@@ -236,11 +203,11 @@ test('keyframes lazy', ({ keyframes, css, tw, sheet }) => {
   // Nothing applied yet
   assert.equal(sheet.target, [])
 
-  assert.is(tw(styles), 'tw-14tbawn')
+  assert.is(tw(styles), 'tw-1u18b6c')
 
   assert.equal(sheet.target, [
-    '@keyframes tw-cm8eaz{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
-    '.tw-14tbawn{animation:1s ease infinite;animation-name:tw-cm8eaz}',
+    '@keyframes tw-w4cbcw{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
+    '.tw-1u18b6c{animation:1s ease infinite;animation-name:tw-w4cbcw}',
   ])
 })
 
@@ -263,10 +230,10 @@ test('animation', ({ animation, tw, sheet }) => {
   // Nothing applied yet
   assert.equal(sheet.target, [])
 
-  assert.is(tw(bounce), 'tw-14tbawn')
+  assert.is(tw(bounce), 'tw-1u18b6c')
   assert.equal(sheet.target, [
-    '@keyframes tw-cm8eaz{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
-    '.tw-14tbawn{animation:1s ease infinite;animation-name:tw-cm8eaz}',
+    '@keyframes tw-w4cbcw{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
+    '.tw-1u18b6c{animation:1s ease infinite;animation-name:tw-w4cbcw}',
   ])
 })
 
@@ -286,10 +253,10 @@ test('animation with callback', ({ animation, tw, sheet }) => {
   // Nothing applied yet
   assert.equal(sheet.target, [])
 
-  assert.is(tw(slidein), 'tw-1go26ky')
+  assert.is(tw(slidein), 'tw-t9bwf7')
   assert.equal(sheet.target, [
-    '@keyframes tw-nlnhc{from{transform:translateX(0%)}to{transform:translateX(100%)}}',
-    '.tw-1go26ky{animation:500ms cubic-bezier(0.4,0,0.2,1);animation-name:tw-nlnhc}',
+    '@keyframes tw-17sq2wg{from{transform:translateX(0%)}to{transform:translateX(100%)}}',
+    '.tw-t9bwf7{animation:500ms cubic-bezier(0.4,0,0.2,1);animation-name:tw-17sq2wg}',
   ])
 })
 
@@ -319,10 +286,10 @@ test('animation object notation', ({ animation, tw, sheet }) => {
   // Nothing applied yet
   assert.equal(sheet.target, [])
 
-  assert.is(tw(bounce), 'tw-1k5i0ed')
+  assert.is(tw(bounce), 'tw-xlkmxq')
   assert.equal(sheet.target, [
-    '@keyframes tw-cm8eaz{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
-    '.tw-1k5i0ed{animation-duration:1s;animation-timing-function:cubic-bezier(0.4,0,0.2,1);animation-iteration-count:infinite;animation-name:tw-cm8eaz}',
+    '@keyframes tw-w4cbcw{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
+    '.tw-xlkmxq{animation-duration:1s;animation-timing-function:cubic-bezier(0.4,0,0.2,1);animation-iteration-count:infinite;animation-name:tw-w4cbcw}',
   ])
 })
 
@@ -345,10 +312,257 @@ test('animation with variant', ({ animation, tw, sheet }) => {
   // Nothing applied yet
   assert.equal(sheet.target, [])
 
-  assert.is(tw`hover:${bounce}`, 'hover:tw-14tbawn')
+  assert.is(tw`hover:${bounce}`, 'hover:tw-1u18b6c')
   assert.equal(sheet.target, [
-    '@keyframes tw-cm8eaz{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
-    '.hover\\:tw-14tbawn:hover{animation:1s ease infinite;animation-name:tw-cm8eaz}',
+    '@keyframes tw-w4cbcw{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}90%{transform:translate3d(0, -4px, 0)}}',
+    '.hover\\:tw-1u18b6c:hover{animation:1s ease infinite;animation-name:tw-w4cbcw}',
+  ])
+})
+
+test('use :global with property callback', ({ tw, sheet }) => {
+  const style = css({
+    ':global': {
+      html: {
+        backgroundColor: ({ theme }) => theme('colors.gray.900'),
+      },
+    },
+  })
+
+  assert.is(tw(style), 'tw-mppuc2')
+
+  assert.equal(sheet.target, ['html{background-color:#111827}'])
+})
+
+test('accepts arrays', ({ tw, sheet }) => {
+  const style = css([
+    {
+      backgroundColor: 'hotpink',
+      '&:hover': {
+        color: 'darkgreen',
+      },
+    },
+    null,
+    {
+      color: 'red',
+    },
+  ])
+
+  assert.is(tw(style), 'tw-185cesv')
+
+  assert.equal(sheet.target, [
+    '.tw-185cesv{background-color:hotpink;color:red}',
+    '.tw-185cesv:hover{color:darkgreen}',
+  ])
+})
+
+test('is variadic', ({ tw, sheet }) => {
+  const style = css(
+    {
+      backgroundColor: 'hotpink',
+      '&:hover': {
+        color: 'darkgreen',
+      },
+    },
+    0,
+    {
+      color: 'red',
+    },
+  )
+
+  assert.is(tw(style), 'tw-185cesv')
+
+  assert.equal(sheet.target, [
+    '.tw-185cesv{background-color:hotpink;color:red}',
+    '.tw-185cesv:hover{color:darkgreen}',
+  ])
+})
+
+test('can be nested', ({ tw, sheet }) => {
+  const style = css({
+    backgroundColor: ({ theme }) => theme('colors.gray.500'),
+    '&:hover': css({
+      color: 'darkgreen',
+    }),
+  })
+
+  assert.is(tw(style), 'tw-m78vn1')
+
+  assert.equal(sheet.target, [
+    '.tw-m78vn1:hover{color:darkgreen}',
+    '.tw-m78vn1{background-color:#6b7280}',
+  ])
+})
+
+test('basic template literal', ({ tw, sheet }) => {
+  const style = css`
+    color: rebeccapurple;
+    background-color: ${({ theme }) => theme('colors.gray.500')};
+    &:hover {
+      ${css`
+        color: darkgreen;
+      `}
+    }
+  `
+
+  assert.is(tw(style), 'tw-u88iva')
+
+  assert.equal(sheet.target, [
+    '.tw-u88iva{color:rebeccapurple;background-color:#6b7280}',
+    '.tw-u88iva:hover{color:darkgreen}',
+  ])
+})
+
+test('nesting in template literal', ({ tw, sheet }) => {
+  const style = css`
+    /* all declarations will be prefixed */
+    padding: 2em 1em;
+    background: papayawhip;
+
+    /* pseudo selectors work as well */
+    &:hover {
+      background: palevioletred;
+    }
+
+    /* media queries are no problem */
+    @media (max-width: 600px) {
+      background: tomato;
+
+      /* nested rules work as expected */
+      &:hover {
+        background: yellow;
+      }
+    }
+
+    > p {
+      /* descendant-selectors work as well, but are more of an escape hatch */
+      text-decoration: underline;
+    }
+
+    /* Contextual selectors work as well */
+    html.test & {
+      display: none;
+    }
+  `
+
+  assert.is(tw(style), 'tw-1io884b')
+
+  assert.equal(sheet.target, [
+    '.tw-1io884b > p{text-decoration:underline}',
+    '.tw-1io884b{padding:2em 1em;background:papayawhip}',
+    '.tw-1io884b:hover{background:palevioletred}',
+    'html.test .tw-1io884b{display:none}',
+    '@media (max-width: 600px){.tw-1io884b:hover{background:yellow}}',
+    '@media (max-width: 600px){.tw-1io884b{background:tomato}}',
+  ])
+})
+
+test('interpolation values', ({ tw, sheet }) => {
+  const random = Math.random()
+
+  const style = css`
+    background: dodgerblue;
+    color: white;
+    border: ${random}px solid white;
+    animation: ${({ theme }) => theme('durations.500')}
+      ${({ theme }) => theme('transitionTimingFunction.in-out')};
+
+    &:focus,
+    &:hover {
+      padding: 1em;
+    }
+
+    .otherClass {
+      margin: 0;
+    }
+
+    :global {
+      body {
+        ${css`
+          color: darkgreen;
+        `}
+      }
+    }
+  `
+
+  // No assert because of random
+  const className = tw(style)
+
+  assert.equal(sheet.target, [
+    'body{color:darkgreen}',
+    `.${className} .otherClass{margin:0}`,
+    `.${className}{background:dodgerblue;color:white;border:${random}px solid white;animation:500ms cubic-bezier(0.4,0,0.2,1)}`,
+    `.${className}:focus, .${className}:hover{padding:1em}`,
+  ])
+})
+
+test('property fallbacks', ({ tw, sheet }) => {
+  const style = css`
+    color: ${['#0000', 'rgba(0, 0, 0, 0)']};
+  `
+
+  assert.is(tw(style), 'tw-11sfiw9')
+
+  assert.equal(sheet.target, ['.tw-11sfiw9{color:#0000;color:rgba(0, 0, 0, 0)}'])
+})
+
+test('omit falsey values', ({ tw, sheet }) => {
+  const style = css`
+    a: ${false};
+    b: ${NaN};
+    c: ${''};
+    d: ${null};
+    e: ${undefined};
+  `
+
+  assert.is(tw(style), 'tw-1dpp5mc')
+
+  assert.equal(sheet.target, [])
+})
+
+test('omit falsey rules', ({ tw, sheet }) => {
+  const style = css`
+    a {
+      ${false}
+    }
+    b {
+      ${NaN}
+    }
+    c {
+      ${''}
+    }
+    d {
+      ${null}
+    }
+    e {
+      ${undefined}
+    }
+  `
+
+  assert.is(tw(style), 'tw-1dpp5mc')
+
+  assert.equal(sheet.target, [])
+})
+
+test('keyframes template literal', ({ tw, sheet }) => {
+  const style = keyframes`
+    from, 20%, 53%, 80%, to {
+      transform: translate3d(0,0,0);
+    }
+    40%, 43% {
+      transform: translate3d(0, -30px, 0);
+    }
+    70% {
+      transform: translate3d(0, -15px, 0);
+    },
+    90% {
+      transform: translate3d(0, -4px, 0);
+    }
+  `
+
+  assert.is(tw(style), 'tw-v87p2q')
+
+  assert.equal(sheet.target, [
+    '@keyframes tw-v87p2q{from, 20%, 53%, 80%, to{transform:translate3d(0,0,0)}40%, 43%{transform:translate3d(0, -30px, 0)}70%{transform:translate3d(0, -15px, 0)}, 90%{transform:translate3d(0, -4px, 0)}}',
   ])
 })
 
--- src/css/index.ts
+++ src/css/index.ts
@@ -1,83 +1,215 @@
-import type { TW, CSSRules, CSSAtKeyframes, InlineDirective, Context, LazyInjected } from '../types'
+import type {
+  TW,
+  CSSRules,
+  CSSAtKeyframes,
+  InlineDirective,
+  LazyInjected,
+  Context,
+  CSSProperties,
+  Falsy,
+} from '../types'
 
 import { tw as defaultTW, hash } from '../index'
+import * as is from '../internal/is'
+import { hyphenate } from '../twind/util'
 
 export interface CSSDirective extends LazyInjected {
-  (context: { tw: TW }): CSSRules
+  (context: Context): CSSRules
 }
 
-const create = <P, R>(
-  factory: (parameter: P, key: string) => InlineDirective,
-): ((parameter: P) => R) => {
-  type Instance = (parameter: P) => R
+export type MaybeThunk<T> = T | ((context: Context) => T)
 
-  // Cache css directive by their JSON stringified value
-  const instances = new WeakMap<TW, Instance>()
+export type MaybeArray<T> = T | readonly T[]
 
-  const build = (tw: TW): Instance => {
-    // Allows to invoke toString() on a directive:
-    // document.body.className = css({ color: 'white' })
-    function toString(this: InlineDirective): string {
-      return tw(this)
-    }
+export interface CSSFactory<T, I, R> {
+  (
+    strings: TemplateStringsArray,
+    ...interpolations: readonly MaybeThunk<MaybeArray<I | string | number | Falsy>>[]
+  ): R
+  (tokens: MaybeThunk<MaybeArray<T | Falsy>>): R
+  (...tokens: readonly MaybeThunk<T | Falsy>[]): R
+}
 
-    const cache = new Map<string, R>()
+function evaluateFunctions(this: TW, key: string, value: unknown): unknown {
+  return is.function(value) ? this(value as InlineDirective) : value
+}
 
-    const evaluateFunctions = (key: string, value: unknown): unknown =>
-      typeof value === 'function' ? tw(value as InlineDirective) : value
+// TODO move to utils
+const evalThunk = <T>(value: MaybeThunk<T>, context: Context): T => {
+  while (is.function(value)) {
+    value = value(context)
+  }
 
-    return (rules: P): R => {
-      const key = JSON.stringify(rules, evaluateFunctions)
+  return value
+}
 
-      let directive = cache.get(key)
+// TODO use utils once tw.apply is merged
+const merge = (target: CSSRules, source: CSSRules, context: Context): CSSRules =>
+  source
+    ? Object.keys(source).reduce((target, key) => {
+        const value = evalThunk(source[key], context)
 
-      if (!directive) {
-        directive = Object.defineProperties(factory(rules, key), {
-          valueOf: {
-            value: toString,
-          },
-          toString: {
-            value: toString,
-          },
-          // Allow twind to generate a unique id for this inline directive
-          // twind uses JSON.stringify which returns undefined for functions like this directive
-          // providing a toJSON function allows to include this directive in the id generation
-          toJSON: {
-            value: () => rules,
-          },
-        }) as R
+        // hyphenate target key only if key is property like (\w-)
+        const targetKey = /^[A-Z0-9-]+$/i.test(key) ? hyphenate(key) : key
 
-        cache.set(key, directive)
+        target[targetKey] =
+          is.object(value) && !Array.isArray(value)
+            ? merge((target[targetKey] || {}) as CSSRules, value as CSSRules, context)
+            : value
 
-        // Ensure the cache does not grow unlimited
-        if (cache.size > 10000) {
-          cache.delete(cache.keys().next().value)
-        }
+        return target
+      }, target)
+    : target
+
+// TODO use once tw.apply is merged
+// eslint-disable-next-line @typescript-eslint/ban-types
+const lazy = <T extends Function>(
+  directive: T,
+  data: unknown,
+  tw: TW | null | undefined | void,
+): T & LazyInjected => {
+  function toString(this: InlineDirective): string {
+    return (tw || defaultTW)(this)
+  }
+
+  return Object.defineProperties(directive, {
+    valueOf: {
+      value: toString,
+    },
+    toString: {
+      value: toString,
+    },
+    // Allow twind to generate a unique id for this directive
+    // twind uses JSON.stringify which returns undefined for functions like this directive
+    // providing a toJSON function allows to include this directive in the id generation
+    toJSON: {
+      value: () => data,
+    },
+  })
+}
+
+const translate = (tokens: unknown[], context: Context): CSSRules => {
+  const collect = (target: CSSRules, token: MaybeThunk<CSSRules>): CSSRules =>
+    Array.isArray(token)
+      ? token.reduce(collect, target)
+      : merge(target as CSSRules, evalThunk(token, context), context)
+
+  return (tokens as MaybeThunk<CSSRules>[]).reduce(collect, {} as CSSRules)
+}
+
+// Based on https://github.com/cristianbote/goober/blob/master/src/core/astish.js
+const newRule = /\s*(?:([\w-%@]+)\s*:?\s*([^{;]+?)\s*(?:;|$|})|([^;}{]*?)\s*{)|(})/gi
+const ruleClean = /\/\*[\s\S]*?\*\/|\s+|\n/gm
+
+const decorate = (selectors: string[], currentBlock: CSSRules): CSSRules =>
+  selectors.reduceRight((rules, selector) => ({ [selector]: rules }), currentBlock)
+
+const saveBlock = (
+  rules: CSSRules[],
+  selectors: string[],
+  currentBlock: CSSRules | undefined | void,
+): void => {
+  if (currentBlock) {
+    rules.push(decorate(selectors, currentBlock))
+  }
+}
+
+const interleave = (
+  strings: TemplateStringsArray,
+  interpolations: unknown[],
+  context: Context,
+): unknown[] => {
+  const result: unknown[] = [strings[0]]
+
+  for (let index = 0; index < interpolations.length; ) {
+    const interpolation = evalThunk(interpolations[index], context)
+
+    if (is.object(interpolation)) {
+      if (interpolation) {
+        result.push(interpolation)
       }
 
-      return directive
+      result.push(strings[++index])
+    } else {
+      // Join consecutive strings
+      // eslint-disable-next-line @typescript-eslint/no-extra-semi
+      ;(result[result.length - 1] as string) += ((interpolation || '') as string) + strings[++index]
     }
   }
 
-  return function (this: TW | null | undefined | void, rules: P): R {
-    const tw = this || defaultTW
-
-    let instance = instances.get(tw)
-
-    if (!instance) {
-      instance = build(tw)
-      instances.set(tw, instance)
-    }
-
-    return instance(rules)
-  }
+  return result
 }
 
-export const css = create<CSSRules, CSSDirective>((rules) => {
-  const plugin: InlineDirective = () => rules
+const astish = (values: unknown[]): CSSRules[] => {
+  // Keep track of active selectors => these are the nested keys
+  const selectors: string[] = []
+  const rules: CSSRules[] = []
 
-  return ({ tw }) => tw(plugin)
-})
+  let currentBlock: CSSRules | undefined | void
+  let match: RegExpExecArray | null
+
+  for (let index = 0; index < values.length; index++) {
+    const value = values[index]
+
+    if (is.string(value)) {
+      while ((match = newRule.exec(value.replace(ruleClean, ' ')))) {
+        if (!match[0]) continue
+
+        // `}` => Save current block
+        if (match[4]) {
+          currentBlock = saveBlock(rules, selectors, currentBlock)
+          selectors.pop()
+        }
+
+        // `... {` => Start a new block
+        if (match[3]) {
+          // selector {
+          currentBlock = saveBlock(rules, selectors, currentBlock)
+          selectors.push(match[3])
+        } else if (!match[4]) {
+          if (!currentBlock) currentBlock = {}
+
+          if (match[2] && /\S/.test(match[2])) {
+            // a) property: value
+            currentBlock[match[1]] = match[2]
+          } else if (values[++index]) {
+            // b) property: ${interpolation}
+            currentBlock[match[1]] = values[index] as CSSRules
+          }
+        }
+      }
+    } else {
+      currentBlock = saveBlock(rules, selectors, currentBlock)
+
+      rules.push(decorate(selectors, value as CSSRules))
+    }
+  }
+
+  saveBlock(rules, selectors, currentBlock)
+
+  return rules
+}
+
+export const css: CSSFactory<CSSRules, CSSRules, CSSDirective> = function (
+  this: TW | null | undefined | void,
+  ...tokens: unknown[]
+): CSSDirective {
+  if (
+    Array.isArray(tokens[0] as TemplateStringsArray) &&
+    Array.isArray((tokens[0] as TemplateStringsArray).raw)
+  ) {
+    // eslint-disable-next-line no-var
+    var strings = tokens[0] as TemplateStringsArray
+    tokens = tokens.slice(1)
+  }
+
+  return lazy(
+    (context: Context): CSSRules =>
+      translate(strings ? astish(interleave(strings, tokens, context)) : tokens, context),
+    tokens,
+    this,
+  )
+}
 
 export interface CSSKeyframes extends LazyInjected {
   (context: Context): string
@@ -107,21 +239,32 @@ export interface CSSKeyframes extends LazyInjected {
  * ```
  * @param waypoints
  */
-export const keyframes = create<CSSAtKeyframes, CSSKeyframes>((waypoints, key) => {
-  const id = hash(key)
+export const keyframes: CSSFactory<
+  CSSAtKeyframes,
+  CSSAtKeyframes | CSSProperties,
+  CSSKeyframes
+> = function (this: TW | null | undefined | void, ...tokens: unknown[]): CSSKeyframes {
+  let id: string
+  const waypoints = css.apply(this, tokens as CSSRules[])
 
-  // tw caches inline plugins by the identity
-  const plugin: InlineDirective = () => ({
-    [`@keyframes ${id}`]: waypoints,
-  })
+  const keyframes: InlineDirective = (context) => {
+    id = hash(JSON.stringify(tokens, evaluateFunctions.bind(context.tw)))
 
-  return ({ tw }) => {
-    // Inject the keyframes
-    tw(plugin)
-    // but return the keyframe id
-    return id
+    return { [`@keyframes ${id}`]: waypoints(context) }
   }
-})
+
+  return lazy(
+    (({ tw }) => {
+      // Inject the keyframes
+      tw(keyframes)
+
+      // but return the keyframe id
+      return id
+    }) as CSSKeyframes,
+    tokens,
+    this,
+  )
+}
 
 /**
  *
@@ -148,7 +291,7 @@ export function animation(
   waypoints: CSSAtKeyframes | CSSKeyframes,
 ): CSSDirective {
   return css.call(this, {
-    ...(typeof value === 'object' ? value : { animation: value }),
-    animationName: typeof waypoints === 'function' ? waypoints : keyframes.call(this, waypoints),
+    ...(is.object(value) ? value : { animation: value }),
+    animationName: is.function(waypoints) ? waypoints : keyframes.call(this, waypoints),
   })
 }
