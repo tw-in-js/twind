6036706681772eb007a63e2c757c6431cb434d63
gojutin <gojutin@gmail.com>
2021-02-26 10:22:09 -0500

    docs: added new components to config

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,8 @@
 import Theme from 'vitepress/theme'
 import Quote from '../components/Quote.vue'
 import UnderConstruction from '../components/UnderConstruction.vue'
+import Emoji from '../components/Emoji.vue'
+import DemoLink from '../components/DemoLink.vue'
 import { setup } from 'twind'
 import 'twind/shim'
 import '../styles/styles.css'
@@ -12,5 +14,7 @@ export default {
   enhanceApp({ app }) {
     app.component('Quote', Quote)
     app.component('UnderConstruction', UnderConstruction)
+    app.component('Emoji', Emoji)
+    app.component('DemoLink', DemoLink)
   },
 }
