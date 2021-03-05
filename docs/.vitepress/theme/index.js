94015b3c7f70059034347d9a0d1ac3c8d37b39b0
gojutin <gojutin@gmail.com>
2021-03-05 15:46:10 -0500

    fix(docs): cleaned up imports

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,22 +1,24 @@
 import Theme from 'vitepress/theme'
+import { setup } from 'twind'
+import '../styles/styles.css'
+import 'twind/shim'
+
+// Components
+import Collapse from '../components/Collapse.vue'
+import DemoLink from '../components/DemoLink.vue'
+import Emoji from '../components/Emoji.vue'
 import Quote from '../components/Quote.vue'
 import UnderConstruction from '../components/UnderConstruction.vue'
-import Emoji from '../components/Emoji.vue'
-import DemoLink from '../components/DemoLink.vue'
-import Collapse from '../components/Collapse.vue'
-import { setup } from 'twind'
-import 'twind/shim'
-import '../styles/styles.css'
 
 setup({ preflight: false })
 
 export default {
   ...Theme,
   enhanceApp({ app }) {
+    app.component('Collapse', Collapse)
+    app.component('DemoLink', DemoLink)
+    app.component('Emoji', Emoji)
     app.component('Quote', Quote)
     app.component('UnderConstruction', UnderConstruction)
-    app.component('Emoji', Emoji)
-    app.component('DemoLink', DemoLink)
-    app.component('Collapse', Collapse)
   },
 }
