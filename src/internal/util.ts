97185159398113173b6a13401637e2135edd59d3
Sascha Tandel <s.tandel@kenoxa.de>
2021-03-25 19:51:46 +0100

    golf: 1 byte ;-)

--- src/internal/util.ts
+++ src/internal/util.ts
@@ -118,9 +118,8 @@ export const buildMediaQuery = (screen: ThemeScreen): string => {
 
 // Based on https://stackoverflow.com/a/52171480
 export const cyrb32: Hasher = (value: string): string => {
-  let h = 9
-
-  for (let index = value.length; index--; ) {
+  // eslint-disable-next-line no-var
+  for (var h = 9, index = value.length; index--; ) {
     h = Math.imul(h ^ value.charCodeAt(index), 0x5f356495)
   }
 
