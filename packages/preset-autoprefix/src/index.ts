1d6fbc129e0fbf5e6fbb726939ef8353a1b5c3b2
SukkaW <isukkaw@gmail.com>
2022-04-22 04:47:36 +0800

    refactor: use stylis in preset-autoprefix

--- packages/preset-autoprefix/src/index.ts
+++ packages/preset-autoprefix/src/index.ts
@@ -7,40 +7,17 @@
 
 import type { Preset } from 'twind'
 
-import { cssPropertyAlias, cssPropertyPrefixFlags, cssValuePrefixFlags } from 'style-vendorizer'
-
-const CSSPrefixFlags = [
-  ['-webkit-', 1 << 0], // 0b001
-  ['-moz-', 1 << 1], // 0b010
-  ['-ms-', 1 << 2], // 0b100
-] as const
+import { prefix } from 'stylis'
 
 export default function presetAutoprefix(): Preset {
   return ({ stringify }) => ({
     stringify(property, value, context) {
-      let cssText = ''
+      /* stylis requires the unprefixed declaration to be ended with ";" */
+      const originalCssText = stringify(property, value, context) + ';'
+      const prefixedCssText = prefix(originalCssText, property.length)
 
-      // Resolve aliases, e.g. `gap` -> `grid-gap`
-      const propertyAlias = cssPropertyAlias(property)
-      if (propertyAlias) cssText += stringify(propertyAlias, value, context) + ';'
-
-      // Prefix properties, e.g. `backdrop-filter` -> `-webkit-backdrop-filter`
-      const propertyFlags = cssPropertyPrefixFlags(property)
-      const valueFlags = cssValuePrefixFlags(property, value)
-
-      for (const prefix of CSSPrefixFlags) {
-        if (propertyFlags & prefix[1]) {
-          cssText += stringify(prefix[0] + property, value, context) + ';'
-        }
-
-        if (valueFlags & prefix[1]) {
-          cssText += stringify(property, prefix[0] + value, context) + ';'
-        }
-      }
-
-      /* Include the standardized declaration last */
-      /* https://css-tricks.com/ordering-css3-properties/ */
-      return cssText + stringify(property, value, context)
+      /* stylis will make sure the standardized declaration is placed at last */
+      return prefixedCssText.endsWith(';') ? prefixedCssText.slice(0, -1) : prefixedCssText
     },
   })
 }
