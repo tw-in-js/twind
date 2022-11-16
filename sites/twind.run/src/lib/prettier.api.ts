fabca7bfefb24ae60df8bbd0b0c744b01787a4b8
Sascha Tandel <s.tandel@kenoxa.de>
2022-11-17 00:09:28 +0100

    fix: reduce initial load size of prettier worker

--- sites/twind.run/src/lib/prettier.api.ts
+++ sites/twind.run/src/lib/prettier.api.ts
@@ -1,32 +1,68 @@
+import type { Options } from 'prettier'
 import type { Prettier } from './prettier'
 
 import prettier from 'prettier/esm/standalone.mjs'
-import parserBabel from 'prettier/esm/parser-babel.mjs'
-import parserHtml from 'prettier/esm/parser-html.mjs'
-import parserMarkdown from 'prettier/esm/parser-markdown.mjs'
-import parserPostcss from 'prettier/esm/parser-postcss.mjs'
-import parserTypescript from 'prettier/esm/parser-typescript.mjs'
+// import parserBabel from 'prettier/esm/parser-babel.mjs'
+// import parserHtml from 'prettier/esm/parser-html.mjs'
+// import parserMarkdown from 'prettier/esm/parser-markdown.mjs'
+// import parserPostcss from 'prettier/esm/parser-postcss.mjs'
+// import parserTypescript from 'prettier/esm/parser-typescript.mjs'
 
 import { Layer } from '../../../../packages/twind/src/internal/precedence'
 
 const defaults = { semi: false, trailingComma: 'all' }
-const plugins = [parserBabel, parserHtml, parserMarkdown, parserPostcss, parserTypescript]
+
+const plugins = [
+  {
+    detect: (parser: string) =>
+      /^[mc]?jsx?$/.test(parser)
+        ? 'babel'
+        : /^[mc]?tsx?$/.test(parser)
+        ? 'babel-ts'
+        : /^json5?$/.test(parser) && parser,
+    load: () => import('prettier/esm/parser-babel.mjs').then((m) => m.default),
+  },
+  {
+    detect: (parser: string) => /^html?$/.test(parser) && 'html',
+    load: () => import('prettier/esm/parser-html.mjs').then((m) => m.default),
+  },
+  {
+    detect: (parser: string) => /^(le|s?c)ss$/.test(parser) && parser,
+    load: () => import('prettier/esm/parser-postcss.mjs').then((m) => m.default),
+  },
+]
+
+async function getOptions(options?: Options) {
+  let parser = options?.parser || /(?:\.([^.]+))?$/.exec(options?.filepath || '')?.[1]
+
+  if (typeof parser === 'string') {
+    for (const plugin of plugins) {
+      const found = plugin.detect(parser)
+      if (found) {
+        return {
+          ...defaults,
+          ...options,
+          parser: found,
+          plugins: [await plugin.load()],
+        }
+      }
+    }
+  }
+
+  return {
+    ...defaults,
+    ...options,
+    plugins: Promise.all(plugins.map((plugin) => plugin.load())),
+  }
+}
 
 const api: Prettier = {
   async format(source, options) {
-    return prettier.format(source, {
-      ...defaults,
-      ...options,
-      plugins,
-    })
+    return prettier.format(source, await getOptions(options))
   },
 
   async formatWithCursor(source, options) {
-    return prettier.formatWithCursor(source, {
-      ...defaults,
-      ...options,
-      plugins,
-    })
+    return prettier.formatWithCursor(source, await getOptions(options))
   },
 
   async formatPreviewCSS(rules) {
