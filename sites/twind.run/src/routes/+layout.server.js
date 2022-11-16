5e7bf5d67a1c2563ae51c5d8b37edda930645980
Sascha Tandel <s.tandel@kenoxa.de>
2022-11-17 00:11:35 +0100

    fix: use permissions-policy header for fullscreen permission of turnstile iframe

--- sites/twind.run/src/routes/+layout.server.js
+++ sites/twind.run/src/routes/+layout.server.js
@@ -6,9 +6,18 @@ export async function load({ setHeaders }) {
       'X-Content-Type-Options': 'nosniff',
       'X-Frame-Options': 'SAMEORIGIN',
       'X-XSS-Protection': '1; mode=block',
-      // 'Permissions-Policy': `document-domain=()`,
-      // TODO: CSP - https://support.cloudflare.com/hc/en-us/articles/216537517-What-is-Content-Security-Policy-CSP-and-how-can-I-use-it-with-Cloudflare-
+      // https://www.permissionspolicy.com
+      'Permissions-Policy': [
+        `cross-origin-isolated=(self)`,
+        `fullscreen=(self "https://challenges.cloudflare.com")`,
+        // `web-share=(self)`,
+        `clipboard-write=(self)`,
+        // `focus-without-user-activation=(self)`,
+        `idle-detection=(self)`,
+        // `unload=(self)`,
+      ].join(', '),
       'Feature-Policy': `fullscreen 'self' https://challenges.cloudflare.com`,
+      // TODO: CSP - https://support.cloudflare.com/hc/en-us/articles/216537517-What-is-Content-Security-Policy-CSP-and-how-can-I-use-it-with-Cloudflare-
     })
   } catch {}
 }
